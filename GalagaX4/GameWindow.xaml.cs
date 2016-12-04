ca30b56e2553f0d23602794ef2c6401d2f153682
Lyrene Labor <Lyrene Labor@LYRENE-PC>
2016-12-04 18:04:31 -0500

    fixed some details

--- GalagaX4/GameWindow.xaml.cs
+++ GalagaX4/GameWindow.xaml.cs
@@ -35,12 +35,8 @@ namespace GalagaX4
             Point playerPoint = new Point(27, 490);
             player = new Player(playerPoint, playerPic, canvas, 15);
 
-            // Level1 lv1 = new Level1(this, canvas, player);
-            //lv1.Play();
-
-             Level3 lv3 = new Level3(this, canvas, player);
-            lv3.Play();
-
+           Level1 lv1 = new Level1(this, canvas, player);
+            lv1.Play();
 
             KeyDown += new KeyEventHandler(MyGrid_KeyDown);
             
