e5d19af4c16c41ed6dbd9f3acb22ee8a4e429d8f
1433278 <1433278@2F14-15>
2016-12-07 17:31:29 -0500

    Boss Commented

--- GalagaX4/Boss.cs
+++ GalagaX4/Boss.cs
@@ -11,6 +11,11 @@ using System.Windows.Threading;
 
 namespace GalagaX4
 {
+    /// <summary>
+    /// The Commander class which implements the Enemies interface defines 
+    /// a concrete implementation of the an Enemy type. The Boss class
+    /// contains the attributes and behavior of a Boss enemy in a galaga game
+    /// </summary>
     class Boss : Enemies
     {
         int hitCounter = 0;
@@ -21,12 +26,26 @@ namespace GalagaX4
         DispatcherTimer timer;
         int moveCounter = 1;
 
+        /// <summary>
+        /// The Boss constructor initializes the attributes of the Boss
+        /// class and the attributes inherited by the Enemies interface. The Boss
+        /// class takes as input a position, an image of the Boss, the game canvas,
+        /// the animation for the commander.
+        /// </summary>
+        /// <param name="point">Position of type Point</param>
+        /// <param name="image">an image of a commander</param>
+        /// <param name="canvas">the game canvas</param>
+        /// <param name="animation">instance of the Animation class</param>
         public Boss(Point point, Image image, Canvas canvas, Animation animation)
             : base(point, image, canvas, animation)
         {
 
         }
-
+        /// <summary>
+        /// The overriden Die method initiates the animation of the explosion of
+        /// the Boss once the Boss is hit 33 times. If the commander is eliminated it stops the
+        /// timer for moving and shooting of the Boss and the game finishes. 
+        /// </summary>
         public override void Die()
         {
             hitCounter++;
@@ -58,12 +77,23 @@ namespace GalagaX4
             }
 
         }
-
+        /// <summary>
+        /// The overridden Fly method takes as input a double for the frequency
+        /// to call the startFly method for the moving of the Boss on the screen
+        /// and the method initiates the animation of the Boss as well
+        /// </summary>
+        /// <param name="frequency">double value for the speed of the moving</param>
         public override void Fly(double frequency)
         {
             Animation.Initiate(this.animation, frequency);
             startFly();
         }
+        /// <summary>
+        /// The startFly method takes as input a double value for frequency of the
+        /// moving of the Boss on the screen and initiates the timer for moving
+        /// with that frequency value
+        /// </summary>
+        /// <param name="frequency">double value for the speed of the moving</param>
         public void startFly()
         {
             //for moving:
@@ -78,14 +108,26 @@ namespace GalagaX4
             this.timer.Tick += new EventHandler(this.updateMoveTimer_Tick);
             this.timer.Start();
         }
-
+        /// <summary>
+        /// The overridden Shoot method initiates the timer for the shooting 
+        /// of the Boss with a specific frequency and calls the Shoot method event 
+        /// handler 
+        /// </summary>
+        /// <param name="frequency"></param>
         public override void Shoot(double frequency)
         {
             timer.Interval = TimeSpan.FromSeconds(frequency);
             timer.Tick += new EventHandler(Shoot);
             timer.Start();
         }
-
+        /// <summary>
+        /// Shoot method event handler sets the isShooting boolean to true
+        /// and creates a Bullet instance and calls the shoot(Left,right and Down) methods of the 
+        /// Bullet class with the right position depending of the Boss's
+        /// position.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void Shoot(Object sender, EventArgs e)
         {
             Point newPoint = new Point();
@@ -115,6 +157,15 @@ namespace GalagaX4
 
 
         }
+        /// <summary>
+        /// The updateMoveTimer_Tick event handler method is called by the startFly method
+        /// with a timer that constantly calls this method. This method handles the 
+        /// movement of the Boss on the screen by making sure that the Boss doesn't
+        /// move pass the limits of the screen while checking for player collision. 
+        /// The method handles as well the random dive sequence of the Boss.
+        /// </summary>
+        /// <param name="sender">object raising the updateMoveTimer_Tick event</param>
+        /// <param name="e">The updateMoveTimer_Tick event raised</param>
 
         private void updateMoveTimer_Tick(object sender, EventArgs e)
         {
@@ -157,7 +208,12 @@ namespace GalagaX4
                 }
             }
         }
-
+        /// <summary>
+        /// The Live method is called if the Boss is hitted but the
+        /// counter is less than 33.It randomly moves the Boss on the screen
+        /// giving an impression that the Boss is disapearing and reapearing
+        /// in another location.
+        /// </summary>
         public void live()
         {
             Animation bossAnimation;
@@ -183,8 +239,13 @@ namespace GalagaX4
                 Animation.Initiate(bossAnimation, 200);
             }
         }
-
-
+        /// <summary>
+        /// The SwitchImage method is used to create the pathern of
+        /// the Boss movement and return the appropriated image of the 
+        /// Boss acording to the number (randomly created) by
+        /// the Live method.
+        /// </summary>
+        /// <returns>An Array with images of the Boss which is uded to animate and give an impression of movements</returns>
         private String[] SwitchImage()
         {
             String[] path = new string[3];
@@ -266,12 +327,18 @@ namespace GalagaX4
             }
             return path;
         }
-
+        /// <summary>
+        /// The Fire method 
+        /// </summary>
         private void fire()
         {
 
         }
-
+        /// <summary>
+        /// The StopMove method will stop the timer and animation
+        /// used to control the Boss. The method is invoked after the 
+        /// Boss dies.
+        /// </summary>
         public void stopMove()
         {
             this.timer.Stop();
