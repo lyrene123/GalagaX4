b2f3266578a4f0578981a07386a1881c57b25bce
1433278 <1433278@2F14-15>
2016-12-07 17:53:14 -0500

    UtilityMethods Class Commented

--- GalagaX4/Utilities.cs
+++ GalagaX4/Utilities.cs
@@ -7,9 +7,19 @@ using System.Windows.Media.Imaging;
 
 namespace GalagaX4
 {
+    /// <summary>
+    /// The UtilityMethods Class defines two methods that perform common
+    /// functions in the Game such as Loading Images and defining the boundaries
+    /// on the screen to avoid the objects to exceed the limites of the Canvas.
+    /// </summary>
     class UtilityMethods
     {
-        // Testing change
+        /// <summary>
+        /// The LoadImage method creates a BitmapImage Object 
+        /// using the supplied relative Uri(Relative path of the Image)
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public static BitmapImage LoadImage(string path)
         {
             BitmapImage src = new BitmapImage();
@@ -19,7 +29,14 @@ namespace GalagaX4
 
             return src;
         }
-        
+        /// <summary>
+        /// The Clamp method define the boundaries of the objects 
+        /// preventing movements that exceeds the limit provided.
+        /// </summary>
+        /// <param name="val"> The base value to be checked</param>
+        /// <param name="min"> The minimun value that is not supposed to be exceeded</param>
+        /// <param name="max"> The max value that is not supposed to be exceeded</param>
+        /// <returns></returns>
         public static double Clamp(double val, double min, double max)
         {
             if (val < min) return min;
