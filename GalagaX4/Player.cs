cc27ba51ba65ce0ce4631e1ea47bc6bacf81c0e5
DanielCavalcanti <danielhbc@gmail.com>
2016-12-07 20:09:33 -0500

    Player Class Commented

--- GalagaX4/Player.cs
+++ GalagaX4/Player.cs
@@ -11,14 +11,22 @@ using System.Windows.Media.Imaging;
 
 namespace GalagaX4
 {
+    /// <summary>
+    /// The Player class which extends the GameObject Class defines 
+    /// all attributes and behavior of the player in a galaga game. 
+    /// </summary>
     class Player : GameObject
     {
         static double coldDown;
         Bullet bullet;
+        //Sound Effects of the bullet and explosion of the player
         public GameSound shootSoundEffect = new GameSound(@"pack://application:,,,/GalagaX4;Component/audio/Firing.wav", true);
         public GameSound explosionSoundEffect = new GameSound(@"pack://application:,,,/GalagaX4;Component/audio/Explosion.wav", true);
         int currentLevel;
-
+        /// <summary>
+        /// The static ColdDown method gets or sets the value 
+        /// of the coldDown of type Double.
+        /// </summary>
         public static double ColdDown
         {
             get { return coldDown; }
@@ -34,7 +42,14 @@ namespace GalagaX4
 
         TextBlock displayPoints;
         int points = 0;
-        
+        /// <summary>
+        /// The Player constructor sets the position of the game element,
+        /// the image and the game canvas from the values inputed into the constructor
+        /// </summary>
+        /// <param name="point">Position of the game elements of type Point</param>
+        /// <param name="image">The image of the game elements </param>
+        /// <param name="canvas">The game canvas in which the game elements is in</param>
+        /// <param name="speed">The Frequency value of type Double</param>
         public Player(Point point, Image image, Canvas canvas
             , double speed) : base(point, image, canvas)
         {
@@ -44,14 +59,24 @@ namespace GalagaX4
             setDisplayPoints();
             updatePoints();
         }
-
+        /// <summary>
+        /// The Player Constructor without parameters to give access
+        /// to useful methods provided by this Class.
+        /// </summary>
         public Player() { }
-
+        /// <summary>
+        /// The GetLives methods return the number of lives of the player.
+        /// </summary>
+        /// <returns>lives of type int</returns>
         public int GetLives()
         {
             return this.lives;
         }
-
+        /// <summary>
+        /// The getEnemiesSize method returns the number of elements contained 
+        /// in the list of enemies.
+        /// </summary>
+        /// <returns>List of type enemies the number of elements contained in the list of enemies.</returns>
         public int getEnemiesSize()
         {
            if(this.enemies == null)
@@ -63,22 +88,33 @@ namespace GalagaX4
                 return this.enemies.Count;
             }
         }
-
+        /// <summary>
+        /// The getEnemiesList method returns the list of enemies created.
+        /// </summary>
+        /// <returns>List of enemies of type Enemies</returns>
         public List<Enemies> getEnemiesList()
         {
             return this.enemies;
         }
-
+        /// <summary>
+        /// The updateCurrentLevel method updates the level of the game.
+        /// </summary>
+        /// <param name="level">A value of type int indicating the level of the game</param>
         public void updateCurrentLevel(int level)
         {
             this.currentLevel = level;
         }
-
+        /// <summary>
+        /// The getCurrentLevel returns the current level of the game.
+        /// </summary>
+        /// <returns>currentlevel A value of type int indicating the level of the game</returns>
         public int getCurrentLevel()
         {
             return this.currentLevel;
         }
-
+        /// <summary>
+        /// The setDisplayLives displays the number of lives on the screen in form of the player image.
+        /// </summary>
         public void setDisplayLives()
         {
            // this.shipLives = new List<Image>(3);
@@ -95,7 +131,10 @@ namespace GalagaX4
                 shipLives[i].Source = UtilityMethods.LoadImage("pics/galaga_ship.png");
             }
         }
-
+        /// <summary>
+        /// The setDisplayPoints method display the number of points gained by the player
+        /// after killing enemies.
+        /// </summary>
         public void setDisplayPoints()
         {
             this.displayPoints = new TextBlock();
@@ -105,23 +144,33 @@ namespace GalagaX4
             this.displayPoints.Foreground = new SolidColorBrush(Colors.White);
             this.displayPoints.FontSize = 20;
         }
-
+        /// <summary>
+        /// The updatePoints method updates the points gained by the player.
+        /// </summary>
         public void updatePoints()
         {
             this.displayPoints.Text = " x "+this.points;
         }
-
+        /// <summary>
+        /// The addCoins method adds points to the player and also invokes the updatePoints method.
+        /// </summary>
+        /// <param name="morePoints">points A value of type int</param>
         public void addCoins(int morePoints)
         {
             this.points += morePoints;
             updatePoints();
         }
-
+        /// <summary>
+        /// The getCoins method returns the number of points.
+        /// </summary>
+        /// <returns>points A value of type int</returns>
         public int getCoins()
         {
             return this.points;
         }
-
+        /// <summary>
+        /// The addLife method adds life to player and sets the image on the screen.
+        /// </summary>
         public void addLife()
         {       
             if (this.lives == 2)
@@ -152,12 +201,18 @@ namespace GalagaX4
             this.points = this.points - 2000;
             updatePoints();
         }
-
+        /// <summary>
+        /// The SetEnemyTarget method sets the List of enemies.
+        /// </summary>
+        /// <param name="enemies"></param>
         public void SetEnemyTarget(List<Enemies> enemies)
         {
             this.enemies = enemies;
         }
-
+        /// <summary>
+        /// The Move method is used to move the player to the left or right according 
+        /// to te user keyboard movements of left arrow or right arrow pressed. 
+        /// </summary>
         public void Move()
         {
             if (Keyboard.IsKeyDown(Key.Left))
@@ -172,7 +227,11 @@ namespace GalagaX4
             this.point.X = UtilityMethods.Clamp(this.point.X, 2, 52);
             Canvas.SetLeft(this.image, this.point.X * speed);
         }
-
+        /// <summary>
+        /// The Shoot method invokes the ShootUpdate method after the player press the Space bar Keyboard
+        /// and also increases the ColdDown element so that the player looses the power to shoot more than
+        /// one bullet at a time.
+        /// </summary>
         public void Shoot()
         {            
             if (this.image.IsLoaded == true)
@@ -184,7 +243,10 @@ namespace GalagaX4
                 }
             }
         }
-
+        /// <summary>
+        /// The ShootUpdate method updates the position of the bullet in the screen
+        /// giving the illusion that the bullet is moving like a real bullet.
+        /// </summary>
         void ShootUpdate()
         {
             double position = Canvas.GetLeft(this.GetImage());
@@ -197,18 +259,29 @@ namespace GalagaX4
             bullet.ShootUp();
             shootSoundEffect.playSound();
         }
-
+        /// <summary>
+        /// The StopShootUp method invokes the the StopShootup method
+        /// from the Bullet Class if the bullet is set to null.
+        /// </summary>
         public void StopShootUp()
         {
             if(bullet != null)
                 bullet.StopShootUp();
         }
-
+        /// <summary>
+        /// The getSpeed method returns the speed frequency.
+        /// </summary>
+        /// <returns>speed A value of type double</returns>
         public double GetSpeed()
         {
             return this.speed;
         }
-
+        /// <summary>
+        /// The overriden Die method initiates the animation of the explosion of
+        /// the Player once shot already. The method also stops the explosion 
+        /// sound effect and dispose its resources invoking the Dispose method
+        /// from the GameSound Class.
+        /// </summary>
         public override void Die()
         {
             decrementLives();
@@ -224,9 +297,13 @@ namespace GalagaX4
             Animation animation = new Animation(this.image, explosions, false, canvas);
             Animation.Initiate(animation, 100);
             explosionSoundEffect.playSound();
+            explosionSoundEffect.Dispose();
             live();
         }
-
+        /// <summary>
+        /// The decrementLives method decrements the lives of the player if the player
+        /// gets hit.
+        /// </summary>
         public void decrementLives()
         {
             if (this.shipLives.Count != 0)
@@ -236,7 +313,10 @@ namespace GalagaX4
             }
             this.lives--;        
         }
-
+        /// <summary>
+        /// The live method creates a new instance image of the player if the lives still greater than 0.
+        /// If the lives is not greater than 0 the explosion of the soundeffect stops and it is Disposed.
+        /// </summary>
         public async void live()
         {              
             if (lives > 0)
